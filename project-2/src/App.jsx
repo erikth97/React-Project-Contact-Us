5f65f8d03a5e7cc6ebaa13fcfa20e131ee69e009
erikth97 <erik.tamayohe@uanl.edu.mx>
2023-06-15 15:25:49 -0600

    stylesnavigation

--- project-2/src/App.jsx
+++ project-2/src/App.jsx
@@ -1,7 +1,10 @@
 import './App.css';
+import Navigation from './components/Navigation';
 
 function App() {
-  return <div></div>;
+  return <div>
+    <Navigation></Navigation>
+  </div>;
 }
 
 export default App;
---
+++ project-2/src/components/Navigation/Navigation.jsx
@@ -0,0 +1,17 @@
+const Navigation = () => {
+  return ( 
+    <nav className="container">
+      <div className="logo">
+        <img src="" alt="logo denso" />
+      </div>
+
+      <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact</li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navigation;
