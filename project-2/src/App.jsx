013fbef345afae7cb49cc7b8963a6ced6dae51ac
erikth97 <erik.tamayohe@uanl.edu.mx>
2023-06-19 15:15:38 -0600

    App

--- project-2/src/App.jsx
+++ project-2/src/App.jsx
@@ -1,12 +1,18 @@
-import '../src/App.css';
-import ContactHeader from './components/ContactHeader/ContactHeader';
-import Navigation from '../src/components/Navigation/Navigation';
+import "./App.css";
+import ContactForm from "./components/ContactForm/ContactForm";
+import ContactHeader from "./components/ContactHeader/ContactHeader";
+import Navigation from "./components/Navigation/Navigation";
 
 function App() {
-  return <div>
-    <Navigation></Navigation>
-    <ContactHeader></ContactHeader>
-  </div>;
+  return (
+    <div>
+      <Navigation />
+      <main className="main_container">
+        <ContactHeader />
+        <ContactForm />
+      </main>
+    </div>
+  );
 }
 
 export default App;
