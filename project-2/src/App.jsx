89fccc9216e351045b102a0fbb3e80cfee8b600d
erikth97 <erik.tamayohe@uanl.edu.mx>
2023-06-19 12:31:48 -0600

    header

--- project-2/src/App.jsx
+++ project-2/src/App.jsx
@@ -1,9 +1,11 @@
 import '../src/App.css';
+import ContactHeader from './components/ContactHeader/ContactHeader';
 import Navigation from '../src/components/Navigation/Navigation';
 
 function App() {
   return <div>
     <Navigation></Navigation>
+    <ContactHeader></ContactHeader>
   </div>;
 }
 
