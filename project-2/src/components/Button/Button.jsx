107553889b9c59145cf9c606cccd98878f5eee1c
erikth97 <erik.tamayohe@uanl.edu.mx>
2023-06-19 14:22:08 -0600

    form

---
+++ project-2/src/components/Button/Button.jsx
@@ -0,0 +1,9 @@
+import { MdMessage } from "react-icons/md";
+
+const Button = () => {
+    return <div>
+    <MdMessage/>
+    Button</div>;
+}
+
+export default Button;
--- project-2/src/components/ContactForm/ContactForm.jsx
+++ project-2/src/components/ContactForm/ContactForm.jsx
@@ -1,9 +1,12 @@
+import styles from "./Contact.module.css";
+
 const ContactForm = () => {
     return (
-        <div>
-            
-        </div>
+        <section className={styles.container}>
+            <div className={styles.contact_form}></div>
+            <div className={styles.contact_image}></div>
+        </section>
     );
-}
+};
 
 export default ContactForm;
